3b8c1e95c17fd6b6d93fd53f62fe6a21f590d91f
CJW <cjw0214@spectory.net>
2025-10-28 11:39:01 +0900

    fix: server.js 수정

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ app.get("/rank", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch rank data" });
   }
 });
+
 app.listen(4000, () => {
-  console.log("Server running on http://localhost:4000");
+  console.log("Server running on mapleStory Api");
 });
